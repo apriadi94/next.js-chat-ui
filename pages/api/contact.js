ae8e91d5241bbb677b232f2c2e8e39c98965600d
apriadi <dev.apriadi@gmail.com>
2021-06-12 19:01:44 +0700

    menambahkan list contact dummy

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,6 +2,7 @@
 
 export default (req, res) => {
     res.status(200).json([
+        { id: 1, name: "Dimas Nugraha", profileImage: "https://picsum.photos/id/10/200/300" },
         { id: 2, name: "Anggara Banyumas", profileImage: "https://picsum.photos/id/15/200/300" },
         { id: 3, name: "Agung", profileImage: "https://i.picsum.photos/id/3/200/300.jpg?hmac=o1-38H2y96Nm7qbRf8Aua54lF97OFQSHR41ATNErqFc" },
         { id: 4, name: "Maria", profileImage: "https://picsum.photos/id/20/200/300" },
